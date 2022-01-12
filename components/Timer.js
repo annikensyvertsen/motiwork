a6489a041c7080b0dd96c89035574012449c27e5
annikensyvertsen <annikensyvertsen@hotmail.com>
2022-01-12 11:11:32 +0100

    a timer that counts down from ten minutes works

--- components/Timer.js
+++ components/Timer.js
@@ -1,37 +1,42 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
-const StopWatch = () => {
+const formatTime = (t) => {
+  console.log("in format time", t)
+  let minutes = Math.floor(t / 60);
+  let seconds = Math.floor(t - minutes * 60)
+  return minutes + "m : " + seconds + " s"
+}
 
-  let padToTwo = (number) => (number <= 9 ? `0${number}`: number);
+export const Timer = () => {
+  const [isRunning, setIsRunning] = useState(false)
+  const [countDownTime, setCountDownTime] = useState(600)
 
-  const [time, setTime] = useState({
-    min: 0,
-    sec: 0,
-    msec: 0
-  })
+  const [formattedTime, setFormattedTime] = useState(formatTime(countDownTime))
+  const [remainingTime, setRemainingTime] = useState(0)
 
-  const [points, setPoints] = useState(0)
+  useEffect(() => {
+    setFormattedTime(formatTime(remainingTime))
 
+  }, [remainingTime])
+
+
+  const onPress = () => { setIsRunning(!isRunning)}
   return (
-    <View style={styles.container}>
-     <View>
-      <Text>{points} p</Text>
-      <Text>Remaining time. </Text>
-      <Text>{padToTwo(time.min) + ' : '}</Text>
-      <Text>{padToTwo(time.sec) + ' : '}</Text>
-     </View>
+    <View>
+          <CountdownCircleTimer
+          isPlaying={isRunning}
+          duration={countDownTime}
+          colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+          colorsTime={[7, 5, 2, 0]}
+          onUpdate={(setRemainingTime)}
+        >
+        {() => <Text>{formattedTime}</Text>}
+      </CountdownCircleTimer>
+      <Button onPress={onPress}>{isRunning ? "Stop" : "Start"}</Button>
     </View>
-  );
-};
-export default StopWatch;
+  )
+}
 
-
-const communityStyles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: 200,
-  },
-})
--- screens/SessionTab.js
+++ screens/SessionTab.js
@@ -1,15 +1,39 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import React, {useState} from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Switch } from "react-native-paper";
 import {StopWatch} from "../components/StopWatch"
+import {Timer} from "../components/Timer"
 
 const SessionTab = () => {
 
+  const [isTimer, setIsTimer] = useState(false)
+
+  const onToggleSwitch = () => setIsTimer(!isTimer)
   return (
     <View>
-     <Text>Session</Text>
-     <StopWatch />
+
+     <Text style={styles.header1}>Session</Text>
+     <View style={styles.container}>
+      <Text>Timer</Text>
+       <Switch color="purple" value={isTimer} onValueChange={onToggleSwitch} />
+      <Text>Stopwatch</Text>
+     </View>
+     {isTimer ? <Timer /> : <StopWatch />}
     </View>
   );
 };
 export default SessionTab;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header1: {
+    fontSize: 18,
+  }
+});
