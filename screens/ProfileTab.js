aefa37cb5f966a2946d876a99172142101accaf4
annikensyvertsen <annikensyvertsen@hotmail.com>
2022-01-14 16:12:10 +0100

    render points in profile page

--- screens/ProfileTab.js
+++ screens/ProfileTab.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text, Image, TextInput } from "react-native";
 import { Button } from "react-native-paper";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 
 const ProfileTab = () => {
-  let currentUser = auth.currentUser;
-  console.log("xu", currentUser);
+  let currentUser = auth.currentUser;  
+  const [userData, setUserData] = useState({uid: currentUser.uid, points: 0 })
+
+  const getUserInformation = () => {
+    db.collection('usersCollection')
+    .doc(currentUser.uid).get().then(doc => {
+      if(doc.exists){
+        setUserData(doc.data())
+      }
+      else {
+        console.log("no user with that id")
+      }
+    }).catch(error =>{
+      console.log("Error getting document; ", error)
+    })
+  }
+  useEffect(() => {
+    getUserInformation()
+    console.log("userdata", userData)
+  }, [])
   return (
     <View>
       <Text>Profile</Text>
       <Text>Email: {currentUser.email}</Text>
       <Text>Username: {currentUser.displayName}</Text>
-
+      <Text>Points: {userData.points}</Text>
       <Button onPress={() => auth.signOut()}>Logg ut</Button>
     </View>
   );
